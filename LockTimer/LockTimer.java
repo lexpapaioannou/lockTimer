c89746670cc3e1c3a04a77bf0db7f0863cb59d04
Lex Papaioannou <apapaioannou@luc.edu>
2017-12-10 11:23:59 -0600

    Started new project.  To do is finish pseudocode.

---
+++ LockTimer/LockTimer.java
@@ -0,0 +1,10 @@
+/*
+	PSEUDO CODE:
+		 This should be a simple code.  A timer runs in the background that resets every week.  A counter is also in the background that resets each week as well.  It starts at an int (i.e. 3) and counts down each time the lock is opened.  Once it reaches 0, it cannot be opened anymore, and you have to wait untill the next week to open it again.
+*/
+
+public class LockTimer {
+	public static void main(String[] args) {
+		
+	}
+}
